76803762ce5e64b4ca9457b112f9027e0fe3e8ed
Liguo Jiao <jiaoliguo888@gmail.com>
2014-12-08 12:51:33 +1300

    other commit
    C# transimiter

---
+++ TCP_Transmiter/Console_TimeClient/Console_TimeClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_TimeClient
+{
+    class Program
+    {
+        private static Socket client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        private static void WhileSend()
+        {
+            while (true)
+            {
+                Console.Write("Enter message: ");
+                string request = Console.ReadLine();
+                byte[] buffer = Encoding.ASCII.GetBytes(request);
+                client_socket.Send(buffer);
+
+                byte[] receiving_buffer = new byte[2046];
+                int receiving = client_socket.Receive(receiving_buffer);
+                byte[] data = new byte[receiving];
+                Array.Copy(receiving_buffer, data, receiving);
+                Console.WriteLine("Received: " + Encoding.ASCII.GetString(data));
+            }
+        }
+
+        private static void WhileConnect()
+        {
+            int counter = 0;
+            while (!client_socket.Connected)
+            {
+                try
+                {
+                    counter++;
+                    client_socket.Connect(IPAddress.Loopback, 100); // port number is 100
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Number of Connection: " + counter.ToString());
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            WhileConnect();
+            WhileSend();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TCP_Transmiter/Console_TimeServer/Console_TimeServer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Console_TimeServer
+{
+    class Program
+    {
+        private static Socket server_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static List<Socket> client_socket = new List<Socket>();
+        private static byte[] buffer = new byte[2048];
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("The server is setting now.....");
+            server_socket.Bind(new IPEndPoint(IPAddress.Any,100));
+            server_socket.Listen(5); //just for 5s
+            server_socket.BeginAccept(new AsyncCallback(AcceptCallback),null);
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = server_socket.EndAccept(AR);
+            client_socket.Add(socket);
+            Console.WriteLine("Clinet Connected");
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            server_socket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] data_buffer = new byte[received];
+            Array.Copy(buffer, data_buffer, received);
+
+            String text = Encoding.ASCII.GetString(data_buffer);
+            Console.WriteLine("Message received: " + text);
+
+            string respons = string.Empty;
+            if (text.ToLower() != "get time")
+            {
+                respons = "Invaild Request";
+            }
+            else 
+            {
+                respons = DateTime.Now.ToLongTimeString();
+            }
+            byte[] data = Encoding.ASCII.GetBytes(respons);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+
+
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+            Console.ReadLine();
+        }
+
+    }
+}
